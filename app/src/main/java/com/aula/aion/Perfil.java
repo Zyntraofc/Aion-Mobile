122f207e6ec8e50cece3c0f1d23a103b1fca584f
Jeff <jefferson.lopes@germinare.org.br>
2025-07-17 19:26:55 -0300

    feat: Adicionando a tela de perfil(xml).

---
+++ app/src/main/java/com/aula/aion/Perfil.java
@@ -0,0 +1,61 @@
+package com.aula.aion;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.aula.aion.databinding.ActivityPerfilBinding;
+
+public class Perfil extends AppCompatActivity {
+
+    ActivityPerfilBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_perfil);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        binding = ActivityPerfilBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.mySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItem = parent.getItemAtPosition(position).toString();
+                binding.mySpinner.setSelection(position);
+                if(selectedItem.equals("Ingles (United States)"))
+                    Toast.makeText(Perfil.this, "Você selecionou: " + selectedItem, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                binding.mySpinner.setSelection(0);
+            }
+        });
+
+        binding.perfilNavBar.btnVoltar.setOnClickListener(view -> {
+            finish();
+        });
+    }
+
+    public void finish() {
+        super.finish();
+        overridePendingTransition(R.anim.stay_still, R.anim.slide_out_right);
+    }
+}
