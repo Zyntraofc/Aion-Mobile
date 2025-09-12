7313fcb53b80cb3e101bab98cc8ea456ef45acd5
ViniAbs <viniabssoaress@gmail.com>
2025-09-12 12:05:56 -0300

    feat: Chamando end-point editar endereco na tela de editar perfil

--- app/src/main/java/com/aula/aion/api/ServiceAPI_SQL.java
+++ app/src/main/java/com/aula/aion/api/ServiceAPI_SQL.java
@@ -1,14 +1,15 @@
 package com.aula.aion.api;
 
-import com.aula.aion.model.ApiCep;
 import com.aula.aion.model.Batida;
 import com.aula.aion.model.Cargo;
+import com.aula.aion.model.Endereco;
 import com.aula.aion.model.Funcionario;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
 import retrofit2.http.Path;
 
 public interface ServiceAPI_SQL {
@@ -22,4 +23,9 @@ public interface ServiceAPI_SQL {
     @POST("/api/v1/batida/inserir")
     Call<Batida> inserirBatida(@Body Batida batida);
 
+    @GET("/api/v1/endereco/buscar/{id}")
+    Call<Endereco> selecionarEnderecoPorId(@Path("id") Long id);
+
+    @PUT("/api/v1/endereco/alterar/{id}")
+    Call<Endereco> alterarEndereco(@Path("id") Long id, @Body Endereco endereco);
 }
---
+++ app/src/main/java/com/aula/aion/model/Endereco.java
@@ -0,0 +1,97 @@
+package com.aula.aion.model;
+
+public class Endereco {
+
+    private Long cdEndereco;
+
+    private String cep;
+
+    private String rua;
+
+    private Integer numero;
+
+    private String complemento;
+
+    private String bairro;
+
+    private String cidade;
+
+    private String estado;
+
+    public Endereco() {}
+
+    public Endereco(Long cdEndereco, String cep, String rua, Integer numero, String complemento, String bairro, String cidade, String estado) {
+        this.cdEndereco = cdEndereco;
+        this.cep = cep;
+        this.rua = rua;
+        this.numero = numero;
+        this.complemento = complemento;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.estado = estado;
+    }
+
+    public Long getCdEndereco() {
+        return cdEndereco;
+    }
+
+    public void setCdEndereco(Long cdEndereco) {
+        this.cdEndereco = cdEndereco;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+}
