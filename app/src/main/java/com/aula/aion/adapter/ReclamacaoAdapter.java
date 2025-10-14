63d530196ac0a2c2f94da1e8bfe27c5c9f33ca33
ViniAbs <viniabssoaress@gmail.com>
2025-10-14 10:21:04 -0300

    feat: chamando end point tpReclamação para poder criar uma reclamaçaõ

--- app/src/main/java/com/aula/aion/adapter/ReclamacaoAdapter.java
+++ app/src/main/java/com/aula/aion/adapter/ReclamacaoAdapter.java
@@ -1,27 +1,39 @@
 package com.aula.aion.adapter;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.aula.aion.api.ServiceAPI_SQL;
 import com.aula.aion.databinding.ItemReclamacaoBinding;
-import com.aula.aion.databinding.ItemReclamacaoBinding;
-import com.aula.aion.model.Reclamacao;
-import com.aula.aion.model.Reclamacao;
+import com.aula.aion.model.TpReclamacao;
 import com.aula.aion.model.Reclamacao;
 
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+
+import okhttp3.Credentials;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ReclamacaoAdapter extends RecyclerView.Adapter<ReclamacaoAdapter.ViewHolder> {
     private List<Reclamacao> reclamacaos;
+    private Retrofit retrofit;
+
 
     public ReclamacaoAdapter(List<Reclamacao> reclamacaos) {
         this.reclamacaos = reclamacaos;
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public class ViewHolder extends RecyclerView.ViewHolder {
         private final ItemReclamacaoBinding binding;
 
         public ViewHolder(ItemReclamacaoBinding binding) {
@@ -29,9 +41,9 @@ public class ReclamacaoAdapter extends RecyclerView.Adapter<ReclamacaoAdapter.Vi
             this.binding = binding;
         }
 
-        public void bind(Reclamacao Reclamacao) {
-            binding.tituloTextView.setText(Reclamacao.getClasssificacao());
-            binding.dataTextView.setText(Reclamacao.getData());
+        public void bind(Reclamacao reclamacao) {
+            buscarTpReclamacao(reclamacao.getCdTpReclamacao(), binding);
+            binding.dataTextView.setText(reclamacao.getReclamacao().format(DateTimeFormatter.ofPattern("dd/MM")));
         }
     }
 
@@ -55,6 +67,42 @@ public class ReclamacaoAdapter extends RecyclerView.Adapter<ReclamacaoAdapter.Vi
 
     public void updateList(List<Reclamacao> novaLista) {
         this.reclamacaos = novaLista;
-        notifyDataSetChanged(); // substituir futuramente por DiffUtil para maior performance
+        notifyDataSetChanged(); 
+    }
+    private void buscarTpReclamacao(Long cdTpReclamacao, ItemReclamacaoBinding binding) {
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(chain -> {
+                    String credentials = Credentials.basic("admin", "123456");
+                    Request request = chain.request().newBuilder()
+                            .addHeader("Authorization", credentials)
+                            .build();
+                    return chain.proceed(request);
+                })
+                .build();
+
+        String url = "https://ms-aion-jpa.onrender.com";
+        retrofit = new Retrofit.Builder()
+                .baseUrl(url)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        ServiceAPI_SQL serviceAPI_SQL = retrofit.create(ServiceAPI_SQL.class);
+
+        serviceAPI_SQL.selecionarTpReclamacaoPorId(cdTpReclamacao).enqueue(new Callback<TpReclamacao>() {
+            @Override
+            public void onResponse(Call<TpReclamacao> call, Response<TpReclamacao> response) {
+                if (response.isSuccessful() && response.body() != null) {
+                    // Atualiza o texto do item específico
+                    binding.tituloTextView.setText(response.body().getNome());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<TpReclamacao> call, Throwable t) {
+                Log.e("API_TpReclamacao", "Erro ao buscar endereço: " + t.getMessage());
+            }
+        });
+
     }
 }
