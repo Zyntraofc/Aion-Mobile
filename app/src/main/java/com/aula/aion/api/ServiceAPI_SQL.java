4a827e9c22018d3fdb5c9338ed07513a57b0b969
ViniAbs <viniabssoaress@gmail.com>
2025-10-14 10:15:19 -0300

    feat: criando novos ens-points para chamadas da API SQL

--- app/src/main/java/com/aula/aion/api/ServiceAPI_SQL.java
+++ app/src/main/java/com/aula/aion/api/ServiceAPI_SQL.java
@@ -4,6 +4,11 @@ import com.aula.aion.model.Batida;
 import com.aula.aion.model.Cargo;
 import com.aula.aion.model.Endereco;
 import com.aula.aion.model.Funcionario;
+import com.aula.aion.model.Reclamacao;
+import com.aula.aion.model.RelatorioPresenca;
+import com.aula.aion.model.TpReclamacao;
+
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
@@ -17,15 +22,32 @@ public interface ServiceAPI_SQL {
     @GET("/api/v1/funcionario/buscar/email/{email}")
     Call<Funcionario> selecionarFuncionarioPorEmail(@Path("email") String email);
 
+    @GET("/api/v1/funcionario/relatorio-presencas/{cdMatricula}")
+    Call<List<RelatorioPresenca>> listarRelatorioPresenca(@Path("cdMatricula") Long cdMatricula);
+
     @GET("/api/v1/cargo/buscar/{id}")
     Call<Cargo> selecionarCargoPorId(@Path("id") Long id);
 
-    @POST("/api/v1/batida/inserir")
-    Call<Batida> inserirBatida(@Body Batida batida);
+    @GET("/api/v1/reclamacao/reclamacao-funcionario/{id}")
+    Call<List<Reclamacao>> selecionarReclamacaoPorFuncionario(@Path("id") Long id);
+
+    @GET("/api/v1/tpReclamacao/listar")
+    Call<List<TpReclamacao>> listarTpReclamacao();
+
+    @GET("/api/v1/tpReclamacao/buscar/{id}")
+    Call<TpReclamacao> selecionarTpReclamacaoPorId(@Path("id") Long id);
 
     @GET("/api/v1/endereco/buscar/{id}")
     Call<Endereco> selecionarEnderecoPorId(@Path("id") Long id);
 
-    @PUT("/api/v1/endereco/alterar/{id}")
+    @POST("/api/v1/batida/inserir")
+    Call<Batida> inserirBatida(@Body Batida batida);
+
+
+    @POST("/api/v1/reclamacao/inserir")
+    Call<Reclamacao> inserirReclamacao(@Body Reclamacao reclamacao);
+
+    @PUT("/api/v1/endereco/atualizar/{id}")
     Call<Endereco> alterarEndereco(@Path("id") Long id, @Body Endereco endereco);
+
 }
