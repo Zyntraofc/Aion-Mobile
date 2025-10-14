233346fa6292dd470c7c70413a95b3251b9c5c53
ViniAbs <viniabssoaress@gmail.com>
2025-10-14 10:17:07 -0300

    feat: ajuste model reclamacao

--- app/src/main/java/com/aula/aion/model/Reclamacao.java
+++ app/src/main/java/com/aula/aion/model/Reclamacao.java
@@ -1,25 +1,77 @@
 package com.aula.aion.model;
 
-public class Reclamacao {
-    private final String classsificacao;
-    private final String descricao;
-    private final String data;
+import java.time.LocalDate;
 
-    public Reclamacao(String classsificacao, String descricao, String data) {
-        this.classsificacao = classsificacao;
+public class Reclamacao {
+
+    private Long cdReclamacao;
+
+    private LocalDate reclamacao;
+
+    private String descricao;
+
+    private Long cdFuncionario;
+
+    private Long cdTpReclamacao;
+
+    private String status;
+
+    public Reclamacao() {}
+
+    public Reclamacao(Long cdReclamacao, LocalDate reclamacao, String descricao, Long cdFuncionario, Long cdTpReclamacao, String status) {
+        this.cdReclamacao = cdReclamacao;
+        this.reclamacao = reclamacao;
         this.descricao = descricao;
-        this.data = data;
+        this.cdFuncionario = cdFuncionario;
+        this.cdTpReclamacao = cdTpReclamacao;
+        this.status = status;
     }
 
-    public String getClasssificacao() {
-        return classsificacao;
+    public Long getCdReclamacao() {
+        return cdReclamacao;
+    }
+
+    public void setCdReclamacao(Long cdReclamacao) {
+        this.cdReclamacao = cdReclamacao;
+    }
+
+    public LocalDate getReclamacao() {
+        return reclamacao;
+    }
+
+    public void setReclamacao(LocalDate reclamacao) {
+        this.reclamacao = reclamacao;
     }
 
     public String getDescricao() {
         return descricao;
     }
 
-    public String getData() {
-        return data;
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Long getCdFuncionario() {
+        return cdFuncionario;
+    }
+
+    public void setCdFuncionario(Long cdFuncionario) {
+        this.cdFuncionario = cdFuncionario;
+    }
+
+    public Long getCdTpReclamacao() {
+        return cdTpReclamacao;
+    }
+
+    public void setCdTpReclamacao(Long cdTpReclamacao) {
+        this.cdTpReclamacao = cdTpReclamacao;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
     }
 }
